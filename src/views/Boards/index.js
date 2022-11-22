65f929ea474c60b48b945b0363b422213e4506b2
raggiolafs00 <ragnaro21@ru.is>
2022-11-22 12:01:28 +0000

    all folder created and data copied over into data.json

--- src/views/Boards/index.js
+++ src/views/Boards/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text, TouchableHighlight, Image } from 'react-native';
-import logo from '../../resources/logo.png';
-import styles from './styles';
+import styles from './/styles';
 
 const Board = () => (
-    <View >
-        <Text>Board</Text>
+    <View style = {styles.container}>
+        <Text style = {styles.text}>Board</Text>
     </View>
 );
 
