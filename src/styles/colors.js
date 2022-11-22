b22755740bdb97b467fe1e21d7f7b645172e2a0e
Karlivar21 <karlk21@ru.is>
2022-11-22 09:59:10 +0000

    Updates

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export const periwinkle =   '#C5D0E6';
+export const darkerBlue =   '#2E3A59';
+export const graniteGray =  '#7F8C8D';
+export const white =        '#FFFFFF';
+export const black =        '#000000';
+export const red =          '#FF0000';
+export const green =        '#00FF00';
+export const blue =         '#0000FF';
+export const yellow =       '#FFFF00';
+export const orange =       '#FFA500';
+export const purple =       '#800080';
+export const brown =        '#A52A2A';
+export const pink =         '#FFC0CB';
+export const gray =         '#808080';
+export const lightGray =    '#D3D3D3';
