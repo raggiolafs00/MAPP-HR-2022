3aa734ada17a9b5e01beb6430bfbbf9cf3c09eab
raggiolafs00 <ragnaro21@ru.is>
2022-11-24 10:36:50 +0000

    TaskModal is now NativeModal and styles changed

--- src/components/TaskModal/index.js
+++ src/components/TaskModal/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View, Text, Modal} from "react-native";
+import { View, Text} from "react-native";
+import NativeModal from "react-native-modal";
 import styles from "./styles";
 import TaskForm from "../TaskForm"
 import {MaterialIcons} from '@expo/vector-icons'
@@ -16,19 +17,22 @@ export default function TaskModal ({isOpen, closeModal, tasks, setTasks, listId}
         task.listId = listId;
         console.log(listId);
         setTasks((currentTasks) => {
+            console.log(currentTasks)
+            console.log(task)
             return [...currentTasks, task];
         })
         closeModal()
     }
 
     return (
-        <Modal visible = {isOpen} 
+        <NativeModal visible = {isOpen} 
                closeModal = {closeModal}
-               animationType = 'slide'>
-            <View style = {styles.modal}> 
+               animationType = 'slide'
+               style={styles.modal}>
+            <View style = {styles.body}> 
                 <TaskForm addTask = {addTask} />
                 <MaterialIcons style = {styles.icon} name = 'close' onPress={closeModal}/>
             </View>
-        </Modal>
+        </NativeModal>
     )
 }
--- src/components/TaskModal/styles.js
+++ src/components/TaskModal/styles.js
@@ -1,5 +1,7 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 import { lightGray } from '../../styles/colors';
+
+const { width: winWidth } = Dimensions.get("window");
 
 export default StyleSheet.create({
     input: {
@@ -17,5 +19,21 @@ export default StyleSheet.create({
         padding: 10,
         borderRadius: 10, 
         alignSelf: 'center'
+    },
+
+    modal: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    body: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexGrow: .3,
+        borderRadius: 10,
+        width: winWidth - 100,
+        backgroundColor: "white",
+        padding: 40,
     }
 })
